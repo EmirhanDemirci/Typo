729913d748c0cff400d75245cc20aa667286db9a
m10k <m.geraets@student.fontys.nl>
2019-11-22 12:39:42 +0100

    Score input
    Dit is De score inputten in tabel op dezelfde mannier als bij reegister. mogelijk met eventueele uiteindelijke trigger aan eind spel.

---
+++ Typo/Controllers/ScoreController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Logic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Typo.Controllers;
+using Typo.Models;
+
+
+namespace Typo.Controllers
+{
+    public class ScoreController : Controller
+    {
+        readonly string _connectionString;
+        public ScoreController(IConfiguration config)
+        {
+            _connectionString = config.GetSection("Connection")["MSSQL"];
+        }
+
+        /*
+        public IActionResult Register()
+        {
+            return View();
+        }*/
+
+        public IActionResult ScoreInput()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult ScoreInput(Score scores)
+        {
+            var query = "INSERT INTO ScoreId1(score) VALUES('{0}')";
+            var queryFull = string.Format(query, scores.score);
+            var sc = new SqlConnection(_connectionString);
+            sc.Open();
+            using (SqlCommand cmd = new SqlCommand(queryFull, sc))
+            {
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    sc.Close();
+                }
+                catch
+                {
+
+                    sc.Close();
+                }
+            }
+            // hoi
+            return RedirectToAction("ScoreInput", "Score");
+        }
+
+
+
+
+
+
+
+        /*
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Account objUser)
+        {
+            var account = LoginUser(objUser);
+            if (account != null)
+            {
+
+                Account.Accounts = account;
+                return RedirectToAction("Index", "Home");
+
+            }
+            return View();
+        }
+
+        private Account LoginUser(Account accounts)
+        {
+            Account account = null;
+            var query = "SELECT * FROM Account WHERE username = '{0}' AND password = '{1}'";
+            var queryFull = string.Format(query, accounts.username, accounts.password);
+            var sc = new SqlConnection(_connectionString);
+            sc.Open();
+            using (SqlCommand cmd = new SqlCommand(queryFull, sc))
+            {
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    account = new Account()
+                    {
+                        userId = reader.GetInt32(0),
+                        username = reader.GetString(1),
+                        password = reader.GetString(2)
+                    };
+                }
+            }
+            return account;
+        }
+
+
+        /*
+        public IActionResult LogOut()
+        {
+            if (Account.Accounts != null)
+            {
+                Account.Accounts = null;
+            }
+
+            return RedirectToAction("Login", "Account");
+        }
+        */
+    
+    }
+}
---
+++ Typo/Models/Score.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Typo.Models
+{
+    public class Score
+    {
+        public int scoreId { get; set; }
+        public string score { get; set; }
+    }
+}
